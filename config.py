b370f03ae8088b0da63bedcdf966d94fb1ffa05b
florzoye <florzoye.official@gmail.com>
2025-11-26 06:01:25 +0700

    config: add gigachat api

--- config.py
+++ config.py
@@ -5,8 +5,11 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-OPENAI_KEY = os.getenv("OPENAI_KEY", "")
+GIGACHAT_KEY  = os.getenv('GIGACHAT_KEY')
 
+PDF_DIR = "data"
+CHROMA_DIR = "data/chroma_db"
+COLLECTION_NAME = "rules"
 
 model_path = "data/model_rnn_bidir.tar"
 chroma_db_path = "data/chroma.sqlite3"
