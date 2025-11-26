6612cdf1beecc01db1ff99cf83c393f5bbef889c
florzoye <florzoye.official@gmail.com>
2025-11-26 17:25:29 +0700

    config: add variables and scope

--- data/config.py
+++ data/config.py
@@ -1,15 +1,17 @@
 import os
+import base64
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-GIGACHAT_KEY  = os.getenv('GIGACHAT_KEY')
+GIGACHAT_AUTH_KEY = os.getenv('GIGACHAT_AUTH_KEY')
+GIGACHAT_SCOPE = "GIGACHAT_API_PERS" 
 
 PDF_DIR = "data"
 CHROMA_DIR = "data/chroma_db"
 COLLECTION_NAME = "rules"
 
 model_path = "data/model_rnn_bidir.tar"
-chroma_db_path = "data/chroma.sqlite3"
+chroma_db_path = "data/chroma_db"
